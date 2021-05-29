e4a305030ac75287e66c9d018b9b141a6bba2167
salman05-oss <63839854+salman05-oss@users.noreply.github.com>
2021-05-29 14:24:18 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,51 @@
+document.getElementById('assign').addEventListener("onclick",log() {
+
+  document.getElementsByClassName('demo').innerHTML = "<div class="row">
+    <div class="col-3" style="margin-top:30px">
+      <ul class="bullet">
+       <li class="ad">
+         <a href="#">Link device</a>
+      </li>
+      <li class="ad">
+       <a href="#">Track Service-man</a>
+      </li>
+      <li class="ad">
+       <a href="#">Edit</a>
+      </li>
+      </ul>
+    </div>
+    <div class="col-9">
+        <h1 style="font-family:Baskerville Old Face; margin-top:30px; font-size:50px">Link Device:</h1>
+        <div class="col-4">
+          <div style="height:30px;">
+          </div>
+          <h3>Type:</h3>
+          <div class="col-6">
+            <select id="Type" style="width:200px">
+              <option value="select">Select an option</option>
+              <option value="Salesman">Salesman</option>
+              <option value="Delivery">Delivery</option>
+            </select>
+          </div>
+        </div>
+            <div class="row">
+            <div class="col-6">
+              <h4>Name</h4>
+              <select id="cars" style="width:500px">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+            </div>
+            <div class="col-6">
+              <h4>Beat</h4>
+              <select id="cars" style="width:500px">
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="opel">Opel</option>
+                <option value="audi">Audi</option>
+              </select>
+            </div>
+          </div>
+}
